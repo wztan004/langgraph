47df58906baa28e226d2ea6e340bd6d2bb176b97
twz <tanwzwz@gmail.com>
2025-06-15 21:03:29 +0800

    add more tutorials

---
+++ tutorial/8-streaming.py
@@ -0,0 +1,58 @@
+# `updates`: show state change
+# `subgraphs=True` show subgraph granularity
+
+from typing import Annotated
+from typing_extensions import TypedDict
+
+from langchain.chat_models import init_chat_model
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver
+
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+from config import models
+from dotenv import load_dotenv
+
+load_dotenv()
+llm = init_chat_model(models.HAIKU3)
+
+# Define subgraph
+class SubgraphState(TypedDict):
+    foo: str  # note that this key is shared with the parent graph state
+    bar: str
+
+def subgraph_node_1(state: SubgraphState):
+    return {"bar": "bar"}
+
+def subgraph_node_2(state: SubgraphState):
+    return {"foo": state["foo"] + state["bar"]}
+
+subgraph_builder = StateGraph(SubgraphState)
+subgraph_builder.add_node(subgraph_node_1)
+subgraph_builder.add_node(subgraph_node_2)
+subgraph_builder.add_edge(START, "subgraph_node_1")
+subgraph_builder.add_edge("subgraph_node_1", "subgraph_node_2")
+subgraph = subgraph_builder.compile()
+
+# Define parent graph
+class ParentState(TypedDict):
+    foo: str
+
+def node_1(state: ParentState):
+    return {"foo": "hi! " + state["foo"]}
+
+builder = StateGraph(ParentState)
+builder.add_node("node_1", node_1)
+builder.add_node("node_2", subgraph)
+builder.add_edge(START, "node_1")
+builder.add_edge("node_1", "node_2")
+graph = builder.compile()
+
+for chunk in graph.stream(
+    {"foo": "foo"},
+    stream_mode="updates",
+    subgraphs=True, 
+):
+    print(chunk)
